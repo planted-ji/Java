862ee666f96d37b9569d7ec1ae99448946c6f6ba
eunji.sim <123479997+planted-ji@users.noreply.github.com>
2024-01-12 14:54:25 +0900

    Update MemberService.java

--- java-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ java-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,13 +3,12 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-/*
-* 회원 가입
-*/
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -18,6 +17,9 @@ public class MemberService {
         this.memberRepository = memberRepository;
     }
 
+    /*
+     * 회원 가입
+     */
     public Long join(Member member) {
         // 같은 이름이 있는 중복 회원 X
         validateDuplicateMember(member); // 중복 회원 검증
